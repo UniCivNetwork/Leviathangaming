42c3fe8f935ee0bd5dbf8ae85dc1c9b9c796251c
UniCivNetwork <54036239+UniCivNetwork@users.noreply.github.com>
2019-12-15 11:53:34 -0500

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -74,10 +74,10 @@ var l_bgImageMapBased = false;
 var l_bgImages = [
 	"image1.jpg",
 	"image2.jpg",
-	"image3.jpg"
-	"image4.jpg"
-	"image5.jpg"
-	"image6.jpg"
+	"image3.jpg",
+	"image4.jpg",
+	"image5.jpg",
+	"image6.jpg",
 ];
 
 // (Image-background only)
